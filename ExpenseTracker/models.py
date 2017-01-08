a2dbeb7e2d3aad41b48fcc161a66c97219975a19
bluman1 <reasonsbluman@gmail.com>
2017-01-08 20:55:39 +0100

    Major Update

--- ExpenseTracker/models.py
+++ ExpenseTracker/models.py
@@ -1,3 +1,99 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.mail import send_mail
 from django.db import models
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, first_name, last_name,
+                     is_staff, is_superuser, is_paying, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        # username = username.strip()
+        email = self.normalize_email(email)
+        user = self.model(
+                          email=email, first_name=first_name, last_name=last_name,
+                          is_staff=is_staff, is_active=True, is_paying=is_paying,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, True,
+                                 **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    A fully featured User model with admin-compliant permissions that uses
+    a full-length email field as the username.
+
+    Username, Email and password are required. Other fields are optional.
+    """
+    # username = models.CharField(_('user name'), max_length=30, unique=True)
+    email = models.EmailField(_('email address'), max_length=254, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin '
+                                               'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+                                    help_text=_('Designates whether this user should be treated as '
+                                                'active. Deselect this instead of deleting accounts.'))
+    is_paying = models.BooleanField(_('paying status'), default=False,
+                                    help_text=_('Designates whether the user is a paying user.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.username)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """" Returns the short name for the user. """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    type = models.CharField(max_length=255, default='Default')
+    cost = models.FloatField
+    description = models.CharField(max_length=255, default='No description.')
+    created = models.DateField(auto_now_add=True)
+    created_detailed = models.DateTimeField(auto_now_add=True)
--- ExpenseTracker/views.py
+++ ExpenseTracker/views.py
@@ -1,41 +1,197 @@
-from django.shortcuts import render_to_response
+import datetime
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from ExpenseTracker.models import User, Expense
 
 
-# Create your views here.
+def validate_email(email):
+    from django.core.validators import validate_email
+    from django.core.exceptions import ValidationError
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False
 
 
-def login(request):
-    return render_to_response("base_login.html")
+def show_home(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            # show user home.
+            user = User.objects.get(email=request.user.email)
+            full_name = user.get_first_name()
+            expenses = Expense.objects.filter(created=datetime.datetime.today()).filter(user=user)
+            total = 0
+            for expense in expenses:
+                total += expense.cost
+            c = {
+                'full_name': full_name,
+                'date': datetime.datetime.today(),
+                'total': total,
+            }
+            return render(request, 'base.html', c)
+        else:
+            # show landing page for anonymous users.
+            return render(request, 'landing_page.html', {})
+    elif request.method == 'POST':
+        # find a workaround for creating multiple expenses simultaneously
+        type_ = request.POST.get('type')
+        cost = request.POST.get('cost')
+        cost = float(cost)
+        description = request.POST.get('description')
+        user = User.objects.get(email=request.user.email)
+        expense = Expense(type=type_, cost=cost, description=description, user=user)
+        expense.save()
+        full_name = user.get_first_name()
+        expenses = Expense.objects.filter(created=datetime.datetime.today()).filter(user=user)
+        total = 0
+        for expense in expenses:
+            total += expense.cost
+        c = {
+            'full_name': full_name,
+            'date': datetime.datetime.today(),
+            'total': total,
+            'msg': '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Success!</strong> Expense Created.</div>'
+        }
+        return render(request, 'base.html', c)
 
 
-def signup(request):
-    return render_to_response("base_signup.html")
+def show_login(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        return render(request, "base_login.html")
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            """return invalid login here"""
+            return render(request, 'base_login.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Failed!</strong> Invalid Details</div>.'})
 
 
-def create_new_expense(request):
-    return render_to_response("base_new_expense.html")
+def show_signup(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        render(request, 'base_signup.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_again = request.POST.get('password_again')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        if not validate_email(email):
+            return render(request, 'base_signup.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Invalid email!</strong> Enter a valid email.</div>.'})
+        if len(password) < 6:
+            return render(request, 'base_signup.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Password too short!</strong> Enter a password longer than that.</div>.'})
+        if password != password_again:
+            return render(request, 'base_signup.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Password mismatch!</strong> Enter matching passwords</div>.'})
+        try:
+            User.objects.get(email=email)
+            render(request, 'base_signup.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Email already exists!</strong> Email already exists</div>.'})
+        except User.DoesNotExist:
+            pass
+        user = User.objects.create_user(email, password, first_name, last_name)
+        auth_user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, auth_user)
+            return HttpResponseRedirect('/')
+        else:
+            """return invalid login here"""
+            return render(request, 'base_signup.html', {'error': '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Failed to login!</strong> Please go to login page</div>.'})
 
 
-def view_day_expense(request):
-    return render_to_response("base_day_expense.html")
+@login_required
+def show_expense_history(request):
+    if request.method == 'GET':
+        user = User.objects.get(email=request.user.email)
+        full_name = user.get_first_name()
+        date = datetime.datetime.today()
+        try:
+            expenses = Expense.objects.filter(created=date).filter(user=user).order_by('created')
+            total = 0
+            for expense in expenses:
+                total += expense.cost
+            c = {
+                'expenses': expenses,
+                'full_name': full_name,
+                'date': date,
+                'total': total
+            }
+            return render(request, 'base_expense_history.html', c)
+        except Expense.DoesNotExist:
+            c = {
+                'full_name': full_name,
+                'date': date,
+                'msg': 'No expenses yet.'
+            }
+            return render(request, 'base_expense_history.html', c)
+    elif request.method == 'POST':
+        date = request.POST.get('date')
+        date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        user = User.objects.get(email=request.user.email)
+        full_name = user.get_first_name()
+        try:
+            expenses = Expense.objects.filter(created=date).filter(user=user).order_by('created')
+            total = 0
+            for expense in expenses:
+                total += expense.cost
+            c = {
+                'expenses': expenses,
+                'full_name': full_name,
+                'date': date,
+                'total': total
+            }
+            return render(request, 'base_expense_history.html', c)
+        except Expense.DoesNotExist:
+            c = {
+                'full_name': full_name,
+                'date': date,
+                'msg': 'No expense was recorded on this date.'
+            }
+            return render(request, 'base_expense_history.html', c)
+
+@login_required
+def show_profile(request):
+    if request.method == 'GET':
+        return render(request, 'base_month_expense.html')
+    elif request.method == 'POST':
+        data = request.POST
+        old_password = data['old_password']
+        new_password = data['new_password']
+        new_password_again = data['new_password_again']
+
+        user = User.objects.get(email=request.user.email)
+        if old_password is not user.password:
+            c = {
+                'errors': 'Your old password is incorrect'
+            }
+            return render(request, 'base_month_expense.html', c)
+        else:
+            if new_password is not new_password_again:
+                c = {
+                    'errors': 'Your new password does not match'
+                }
+                return render(request, 'base_month_expense.html', c)
+            else:
+                user.password = new_password
+                user.save()
+                c = {
+                    'message': 'Password successfully changed'
+                }
+                return render(request, 'base_month_expense.html', c)
 
 
-def view_week_expense(request):
-    return render_to_response("base_week_expense.html")
-
-
-def view_month_expense(request):
-    return render_to_response("base_month_expense.html")
-
-
-def view_expense_history(request):
-    return render_to_response("base_expense_history.html")
-
-
-def view_about(request):
-    pass
-
-
-def view_profile(request):
-    pass
-
+@login_required
+def show_logout(request):
+    if request.method == 'GET':
+        logout(request)
+        return HttpResponseRedirect('/')
